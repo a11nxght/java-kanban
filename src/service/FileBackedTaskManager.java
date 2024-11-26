3ebf1efe031a044fcd3dc7ba5957c5a099baccec
a11nxght <a11nxght@yandex.kz>
2024-11-27 00:46:07 +0300

    Написать тесты для методов, которые работают с файлами. Исправить ошибки.

--- src/service/FileBackedTaskManager.java
+++ src/service/FileBackedTaskManager.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
     final Path path;
