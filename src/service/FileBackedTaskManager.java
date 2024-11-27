9e214825d8f2dd71907361cf968f23c03c152c53
a11nxght <a11nxght@yandex.kz>
2024-11-28 00:16:19 +0300

    Исправить замечания.

--- src/service/FileBackedTaskManager.java
+++ src/service/FileBackedTaskManager.java
@@ -8,8 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
     final Path path;
